3ba63e5405bc2a877a89ebf6698199c20e4c59cf
sainath <saiminkikar@yahoo.com>
2023-03-12 16:49:08 +0530

    first commit

---
+++ src/functions/factorial.java
@@ -0,0 +1,28 @@
+package functions;
+
+public class factorial {
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    /*
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+
+     */
+
+    //binomial coefficient
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = a-b;
+        int d = (factorial(a)/(factorial(b)*factorial(c)));
+        System.out.println(d);
+    }
+}
---
+++ src/functions/product.java
@@ -0,0 +1,13 @@
+package functions;
+
+public class product {
+    public static void main(String[] args) {
+        int a = 65;
+        int b = 45;
+        pro(a,b);
+    }
+    public static void pro(int a , int b){
+        int prod = a*b;
+        System.out.println("product = " + prod);
+    }
+}
