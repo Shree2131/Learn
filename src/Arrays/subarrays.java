cdd5ba03498d7df6420fb0667bfdacd3a986b9e3
sainath <saiminkikar@yahoo.com>
2023-03-19 02:52:06 +0530

    adding code

--- src/Arrays/subarrays.java
+++ src/Arrays/subarrays.java
@@ -6,17 +6,20 @@ public class subarrays {
        subarr(nums);
     }
     public static void subarr(int nums[]){
-
+      int sum = 0 , maxsum = Integer.MIN_VALUE;
         for (int i = 0; i <= nums.length-1; i++) {
             for (int j = 0; j <= nums.length-1; j++) {
-                int sum = 0;
+                sum = 0;
                 for (int k = i; k <= j; k++) {
                     System.out.print(nums[k]+" ");
                     sum = sum +nums[k];
                 }
                 System.out.println(" sum = "+sum);
-            }
-            System.out.println();
+                if (maxsum < sum){
+                    maxsum = sum;
+                }
+            }System.out.println();
         }
+        System.out.println("maxsum = "+maxsum);
     }
 }
