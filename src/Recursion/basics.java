94bccb248656935d9c839459dcfab5ff3adbda37
sainath <saiminkikar@yahoo.com>
2023-03-29 01:05:14 +0530

    adding code

--- src/Recursion/basics.java
+++ src/Recursion/basics.java
@@ -3,14 +3,22 @@ package Recursion;
 public class basics {
     public static void main(String[] args) {
         int n = 10;
-        printdec(n);
+        printInc(n);
     }
-    public static void printdec(int n){
+    public static void printDec(int n){
         if (n == 1){
             System.out.println(1);
             return;
         }
         System.out.println(n);
-        printdec(n-1);
+        printDec(n-1);
+    }
+    public static void printInc(int n){
+        if (n == 1){
+            System.out.println(1);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n);
     }
 }
